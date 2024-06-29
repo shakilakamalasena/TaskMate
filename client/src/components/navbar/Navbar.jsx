6f2cd2b3b578e941e4ce753259c15a4cbdbafb31
Shakila Kamalasena <104765058+shakilakamalasena@users.noreply.github.com>
2024-06-29 16:58:22 +0530

    Navigation bar done

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./navbar.scss";
+
+const Navbar = () => {
+    const [open, setOpen] = useState(false);
+    return (
+        <div>
+            <nav>
+                <div className="left">
+                    <a href="/" className="logo">
+                        <img src="/logo.png" alt="logo" />
+                        <span>TaskMate</span>
+                    </a>
+                    <a href="/">Home</a>
+                    <a href="/">About</a>
+                    <a href="/">Contacts</a>
+                    <a href="/">Agents</a>
+                </div>
+                <div className="right">
+                    <a href="/">Sign in</a>
+                    <a href="/" className="register">
+                        Sign up
+                    </a>
+                    <div className="menuIcon">
+                        <img
+                            src="/menu.png"
+                            alt=""
+                            onClick={() => setOpen((prev) => !prev)}
+                        />
+                    </div>
+                    <div className={open ? "menu active" : "menu"}>
+                        <a href="/">Home</a>
+                        <a href="/">About</a>
+                        <a href="/">Contacts</a>
+                        <a href="/">Agents</a>
+                        <a href="/">Sign in</a>
+                        <a href="/">Sign up</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
