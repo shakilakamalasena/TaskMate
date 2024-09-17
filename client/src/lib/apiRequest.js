5399fdc93d9c684d27c8b97205aeb5b58d65b8e5
Shakila Kamalasena <104765058+shakilakamalasena@users.noreply.github.com>
2024-09-18 01:05:15 +0530

    Update API base URL to use the deployed backend URL

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: "http://localhost:8800/api",
+    baseURL: "https://taskmate-api.vercel.app/api",
     withCredentials: true,
 });
 
